ad25715c3a318c1d55e00d9e7d7a7c3093817176
DaffaAhmadSM <Daffamubaroqi321@gmail.com>
2025-08-15 17:59:52 +0700

    feat deployment setup

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+# check if .env exists, if not copy from .env.example
+if [ ! -f .env ]; then
+    echo "Copying .env.example to .env"
+    cp .env.example .env
+else
+    echo ".env already exists, skipping copy"
+fi
+
+docker compose build
+
+docker run --rm -it --volume $(pwd):/app ponpes-absensi-be composer install --no-dev --optimize-autoloader
+docker run --rm -it --volume $(pwd):/app ponpes-absensi-be php artisan key:generate
+
+docker run --rm -it --volume $(pwd):/app ponpes-absensi-be php artisan octane:install --server=frankenphp
+
+# up composer
+docker compose up -d
+docker compose exec api php artisan migrate
+docker compose ps
