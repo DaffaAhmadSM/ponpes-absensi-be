651e5a392a34d82595108f28c0bafaaa8610d92a
DaffaAhmadSM <Daffamubaroqi321@gmail.com>
2025-08-29 17:10:05 +0700

    fix bug can't edit instance location

--- database/migrations/2025_08_02_061107_create_instance_locations_table.php
+++ database/migrations/2025_08_02_061107_create_instance_locations_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -15,7 +14,7 @@ return new class extends Migration
             $table->id();
             $table->string('name')->unique();
             $table->decimal('longitude', 11, 8);
-            $table->decimal('latitude', 10, 8);
+            $table->decimal('latitude', 11, 8);
             $table->boolean('default')->default(false)->index();
             $table->string('address')->nullable();
             $table->text('description')->nullable();
